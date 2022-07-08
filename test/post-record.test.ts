6dcf9a28a1dd560978566b9bf6cadc504af31c13
Grzegorz Strzelecki <grzegorz.strzelecki84@gmail.com>
2022-07-08 20:31:35 +0200

    Added Jest and first tests to project.

---
+++ test/post-record.test.ts
@@ -0,0 +1,27 @@
+import { PostRecord } from "../records/post.record";
+
+const defaultObj = {
+  id: 'abc',
+  tags: ['aaa','bbb'],
+  likeCount: 0,
+  createdAt: new Date(),
+  author: 'Test author',
+  title: "Test tilte",
+  message: 'Test message',
+  selectedFile: 'test_file.png'
+}
+
+test('Can build PostRecord', () => {
+  const post = new PostRecord(defaultObj);
+
+  expect(post.id).toBe('abc');
+  expect(post.tags).toContain('aaa');
+  expect(post.tags).toContain('bbb');
+});
+
+test('Validates empty author', () => {
+  expect(() => new PostRecord({
+    ...defaultObj,
+    author: '',
+  })).toThrow('Author cannot be empty or exceed 50 chars.');
+});
